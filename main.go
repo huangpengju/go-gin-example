26dd38a2cdb5d52f8751ba4402d040a7a033a486
huangpengju <15713716933@163.com>
2023-08-09 17:33:09 +0800

    优化代码注释

--- main.go
+++ main.go
@@ -12,13 +12,17 @@ func main() {
 	// router := gin.Default()
 	// // 创建不同的 HTTP 方法绑定到 Handles 中
 	// // gin.Context 是 gin 中的上下文，
+	// 可以为每个路由添加任意数量的中间件
 	// router.GET("/test", func(c *gin.Context) {
 	// 	c.JSON(200, gin.H{ // gin.H 就是一个map[sting]interface{}
 	// 		"message": "test",
 	// 	})
 	// })
 
+	// 接收配置好中间件的路由
 	router := routers.InitRouter()
+
+	// 创建一个 服务器 对象
 	s := &http.Server{
 		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
 		Handler:        router,
@@ -26,6 +30,8 @@ func main() {
 		WriteTimeout:   setting.WriteTimeout,
 		MaxHeaderBytes: 1 << 20,
 	}
-
+	// 调用 Server 对象 ListenAndServe() 方法
+	// 会使用网络库提供的 net.Listen 监听对应地址上的 TCP
+	// 连接并通过 net/http.Server.Serve 处理客户端的请求
 	s.ListenAndServe()
 }
--- routers/router.go
+++ routers/router.go
@@ -7,30 +7,42 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// InitRouter 初始化路由
+// 接收配置好中间件的路由
 func InitRouter() *gin.Engine {
 	//  New返回一个新的空白引擎实例，没有附加任何中间件。
+	// 创建一个没有任何默认中间件的路由
 	r := gin.New()
 
-	r.Use(gin.Logger())
+	// 全局中间件
+	// Logger 中间件将日志写入 gin.DefaultWriter，即使你将 GIN_MODE 设置 release。
+	// By default gin.DefaultWriter = os.Stdout
+	// Use 将全局中间件连接到路由器
+	// 给 r 使用 Logger 中间件
+	r.Use(gin.Logger()) // 给 r 注册一个全局中间件
+	// Recovery 中间件会 recovery 任何 panic。如果有 panic 的话，会写入 500.
+	// 给 r 使用() Recover 中间件
+	r.Use(gin.Recovery()) // 给 r 注册一个全局中间件
 
-	r.Use(gin.Recovery())
-
-	// 设置模式 为 debug
+	// 设置模式为 debug  // 	除了 debug 还有 test 和 release 模式
 	gin.SetMode(setting.RunMode)
 
+	// 认证路由组
+	//
 	apiv1 := r.Group("/api/v1")
+	// 路由组中间件！ 在 apiv1 路由组中使用自定义创建的中间件
 	{
 		// 获取标签列表
-		apiv1.GET("/tags", v1.GetTags)
+		apiv1.GET("/tags", v1.GetTags) // 为某个路由单独注册中间件
 
 		// 新建标签
-		apiv1.POST("/tags", v1.AddTag)
+		apiv1.POST("/tags", v1.AddTag) // 为某个路由单独注册中间件
 
 		// 更新指定标签
-		apiv1.PUT("/tags/:id", v1.EditTag)
+		apiv1.PUT("/tags/:id", v1.EditTag) // 为某个路由单独注册中间件
 
 		// 删除指定标签
-		apiv1.DELETE("/tags/:id", v1.DeleteTag)
+		apiv1.DELETE("/tags/:id", v1.DeleteTag) // 为某个路由单独注册中间件
 	}
 	// r.GET("/test", func(c *gin.Context) {
 	// 	c.JSON(200, gin.H{
