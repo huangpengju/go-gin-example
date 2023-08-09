06f2538347130369dc94638a0b930d6999d37f26
huangpengju <15713716933@163.com>
2023-08-09 11:33:24 +0800

    优化代码注释

--- models/tag.go
+++ models/tag.go
@@ -1,3 +1,6 @@
+// models 包处理 tag 模型与数据库的映射
+// 包中 GetTags 获取多个文章标签 Tag
+// 包中 GetTagTotal 用于获取 Tag 的数量
 package models
 
 type Tag struct {
@@ -9,16 +12,36 @@ type Tag struct {
 	State      int    `json:"state"`
 }
 
-// 获取多个文章标签
+// GetTags 获取多个文章标签 Tag
+// 参数 pageNum 表示跳过多少条数据
+// 参数 pageSize 表示每页显示多少条数据
+// 参数 maps 表示查询条件参数（含name、state）
+// 返回值 tags 表示 Tag 列表
 func GetTags(pageNum int, pageSize int, maps interface{}) (tags []Tag) {
-	// Limit 指定要检索的最大记录数
-	// Offset 指定在开始返回记录之前要跳过的记录数
+	// Where 查询条件
+	// db.Where(map[string]interface{}{"name": "jinzhu", "age": 20}).Find(&users)
+	// SELECT * FROM users WHERE name = "jinzhu" AND age = 20;
+
+	// Offset 指定在开始返回记录之前要跳过的记录数(表示跳过多少条数据)
+	// Limit 指定要检索的最大记录数(表示每页显示多少条数据)
+	// Find 获取所有记录
+
 	db.Where(maps).Offset(pageNum).Limit(pageSize).Find(&tags)
+	// SELECT * FROM tags OFFSET ? LIMIT ? WHERE name=? AND state=?;
 	return
 }
 
+// GetTagTotal 用于获取 Tag 的数量
+// 参数 maps 表示查询条件参数（含name、state）
+// 返回值 count 表示 Tag 的总数量
 func GetTagTotal(maps interface{}) (count int) {
+	// Model() 指定要运行数据库操作的模型
+	// Where 查询条件
 	// Count 用于获取匹配的记录数
+	// db.Model(&User{}).Where("name = ?", "jinzhu").Count(&count)
+	// SELECT count(*) FROM users WHERE name = 'jinzhu';
+
 	db.Model(&Tag{}).Where(maps).Count(&count)
+	// SELECT count(*) FROM tags WHERE name=? AND state=?
 	return
 }
--- routers/api/v1/tag.go
+++ routers/api/v1/tag.go
@@ -14,10 +14,17 @@ import (
 
 // 获取多个文章标签
 func GetTags(c *gin.Context) {
+	// 参数说明
+	// name
+	// state
+	// page  当前页
+
 	// 查询参数 name
 	name := c.Query("name")
 
+	// maps 存放查询参数
 	maps := make(map[string]interface{})
+	// data 存放返回数据
 	data := make(map[string]interface{})
 
 	if name != "" {
@@ -25,16 +32,21 @@ func GetTags(c *gin.Context) {
 	}
 
 	var state int = -1
+	// 查询参数 state
 	if arg := c.Query("state"); arg != "" {
-		state = com.StrTo(arg).MustInt()
+		state = com.StrTo(arg).MustInt() // string 转 int
 		maps["state"] = state
 	}
 
 	code := e.SUCCESS
+	// 获取 Tag 列表
 	data["lists"] = models.GetTags(util.GetPage(c), setting.PageSize, maps)
+	// 获取 Tag 总数
 	data["total"] = models.GetTagTotal(maps)
 
-	c.JSON(http.StatusOK, gin.H{
+	// JSON将给定的结构作为JSON序列化到响应体中。
+	// 它还将Content-Type设置为“application/json”。
+	c.JSON(http.StatusOK, gin.H{ // H 是map[string]interface{} 类型
 		"code": code,
 		"msg":  e.GetMsg(code),
 		"data": data,
