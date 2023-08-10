047be24cec7e59ea375d1a375c4d35e4a329af98
huangpengju <15713716933@163.com>
2023-08-10 23:15:22 +0800

    文章的增删改查

--- models/article.go
+++ models/article.go
@@ -68,6 +68,18 @@ func EditArticle(id int, data map[string]interface{}) bool {
 	return true
 }
 
+func AddArticle(data map[string]interface{}) bool {
+	db.Create(&Article{
+		TagID:     data["tag_id"].(int),
+		Title:     data["title"].(string),
+		Desc:      data["desc"].(string),
+		Content:   data["content"].(string),
+		CreatedBy: data["created_by"].(string),
+		State:     data["state"].(int),
+	})
+	return true
+}
+
 func DeleteArticle(id int) bool {
 	db.Where("id = ?", id).Delete(Article{})
 	return true
